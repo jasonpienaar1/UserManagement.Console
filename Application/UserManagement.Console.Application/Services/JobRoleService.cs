605184ac1b63893b173011e09823c5e67de66be1
JJ Swart <40439140+Swartjaak@users.noreply.github.com>
2024-02-16 07:15:32 +0200

    Add project files.

---
+++ Application/UserManagement.Console.Application/Services/JobRoleService.cs
@@ -0,0 +1,12 @@
+﻿namespace UserManagement.Console.Application.Services
+{
+  /// <summary>
+  /// The Job Role Service
+  /// </summary>
+  /// <remarks>
+  /// All related action to job role will be handled here.
+  /// </remarks>
+  public class JobRoleService
+  {
+  }
+}
---
+++ UserManagement.Console.Shared/Models/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿namespace UserManagement.Console.Shared.Models
+{
+  /// <summary>
+  /// The Menu Item class.
+  /// </summary>
+  /// <remarks>
+  /// Tip: Use LINQ to get and validate the menu the selected item.
+  /// </remarks>
+  public class MenuItem
+  {
+    /// <summary>
+    /// Gets or sets the Menu Item Id.
+    /// </summary>
+    /// <remarks>
+    /// You need to generate the menu item id.
+    /// </remarks>
+    public int MenuItemId { get; set; }
+
+    public string MenuItemDescription { get; set; } = string.Empty;
+  }
+}
